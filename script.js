9ea320cbb65a505f7da8b80c1ae34c6ba4414e32
bulhoesgabriel <bulhoesgabriel@outlook.com>
2025-01-20 17:06:08 -0300

    resume udpate
    added my resume available for download and some animate.css effects on it

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const paragrafo1 = document.getElementById('paragrafo1')
 const paragrafo2 = document.getElementById('paragrafo2')
 const paragrafo3 = document.getElementById('paragrafo3')
 const paragrafo4 = document.getElementById('paragrafo4')
+const cv = document.getElementById('cv')
 
 function toggleLightMode() {
     body.classList.toggle('light-mode')
@@ -22,14 +23,17 @@ sobre.addEventListener('click', () => {
     paragrafo2.classList.remove('animate__fadeInDown')
     paragrafo3.classList.remove('animate__fadeInDown')
     paragrafo4.classList.remove('animate__fadeInDown')
+    cv.classList.remove('animate__bounceInLeft')
     void titulo.offsetWidth
     void paragrafo1.offsetWidth
     void paragrafo2.offsetWidth
     void paragrafo3.offsetWidth
     void paragrafo4.offsetWidth
+    void cv.offsetWidth
     titulo.classList.add('animate__slideInLeft')
     paragrafo1.classList.add('animate__fadeInDown')
     paragrafo2.classList.add('animate__fadeInDown')
     paragrafo3.classList.add('animate__fadeInDown')
     paragrafo4.classList.add('animate__fadeInDown')
+    cv.classList.add('animate__bounceInLeft')
 })
